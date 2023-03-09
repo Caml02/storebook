173c219e63a5b131446117a6ac6988eece09a369
Cristian Morales <cristianmorales3006@gmail.com>
2023-03-09 14:49:37 -0700

    settings.py

--- storebook/settings.py
+++ storebook/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'libreria',
         'USER': 'root',
-        'PASSWORD': 'sAurio15!',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
